4ad193d4111f19e580ae652308428f5027a43323
Evan <git@erosson.org>
2020-03-03 02:18:15 -0500

    dev contact info

--- src/Page/Changelog.elm
+++ src/Page/Changelog.elm
@@ -11,7 +11,18 @@ view : Datamine -> { m | changelog : String } -> List (Html msg)
 view dm m =
     [ small []
         [ p []
-            [ text "WolcenDB based on Wolcen build revision "
+            [ text "Contact the developer: "
+            , a [ href "https://keybase.io/erosson", target "_blank" ] [ text "Keybase chat" ]
+            , text ", "
+            , a [ href "https://gitlab.com/erosson/wolcendb/issues/new", target "_blank" ] [ text "GitLab issue" ]
+            , text ", or "
+            , a [ href "https://www.reddit.com/message/compose/?to=kawaritai&subject=WolcenDB", target "_blank" ] [ text "Reddit" ]
+            , text "."
+            ]
+        ]
+    , small []
+        [ p []
+            [ text "Wolcen build revision "
             , code [] [ text dm.revision.buildRevision ]
             , text ", created at "
             , code [] [ text dm.revision.date ]
