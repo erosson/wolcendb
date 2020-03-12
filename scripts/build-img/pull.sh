8447c62a4b279bd30aa781631e1d431be3ce2e37
Evan <git@erosson.org>
2020-03-12 02:47:23 -0400

    fix pull

--- scripts/build-img/pull.sh
+++ scripts/build-img/pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
 cd "`dirname "$0"`/../.."
-aws s3 sync s3://img-wolcendb.erosson.org/datamine ./build-img2/datamine/
+aws s3 sync s3://img-wolcendb.erosson.org/datamine ./build-img/datamine/
 
 bash ./scripts/public/buildRevisions.sh
